acc5d9274ee19747fe7febe1863311efdc03e4ef
Gabriel Azeredo <gabriel21azeredo@gmail.com>
2022-05-27 14:10:32 -0300

    Adicionado model Todo

---
+++ Models/Todo.cs
@@ -0,0 +1,8 @@
+namespace Models.Todo
+{
+    public class Todo{
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public bool Done { get; set; }
+    }   
+}
