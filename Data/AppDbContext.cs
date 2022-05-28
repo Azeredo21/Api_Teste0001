c2cc2f14e5386eaba29c7c041104bc83a21becce
Gabriel Azeredo <gabriel21azeredo@gmail.com>
2022-05-28 13:10:22 -0300

    Add DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Api_Teste0001.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Teste0001.Data {
+    public class AppDbContext : DbContext {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+            optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+    }
+}
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,8 +1,12 @@
-namespace Models.Todo
+
+using System;
+
+namespace Api_Teste0001.Models
 {
     public class Todo{
         public int Id { get; set; }
         public string Tittle { get; set; }
         public bool Done { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
     }   
 }
